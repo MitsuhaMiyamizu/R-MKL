8c930e67f831ab4c33345ef3badfeb350526caf4
MitsuhaMiyamizu <kiritoalex@live.com>
2020-09-28 02:19:51 +0000

    Start building package and change the way of compression

--- parallel.R
+++ parallel.R
@@ -8,7 +8,7 @@ registerDoParallel()
 set.seed(100)
 
 system.time(foreach (i = 1:4) %dopar% {
-            a = replicate(10000, rnorm(10000))
+            a = replicate(100, rnorm(1000))
             d = determinant(a)})
 
 doParallel::stopImplicitCluster()
